9ce305a0a2eb9fdb025243a65c9f902da25821d6
Yaskuzin <diman-y96@mail.ru>
2018-05-12 13:08:50 +0300

    h

--- include/PrimaryGen.hh
+++ include/PrimaryGen.hh
@@ -7,16 +7,37 @@
 
 #include <G4VUserPrimaryGeneratorAction.hh>
 #include <G4ParticleGun.hh>
+#include <G4UImessenger.hh>
+#include <G4UIcmdWithADouble.hh>
+
+class Pormes;
 
 class PrimaryGen: public G4VUserPrimaryGeneratorAction{
 private:
     G4ParticleGun* gun;
+    G4double Neutron;
     G4ThreeVector pos_vect;
+    G4int i;
 public:
+    Pormes* Pormesclass;
+    void Porval(G4double porval);
     PrimaryGen();
 
     virtual ~PrimaryGen();
 
     void GeneratePrimaries(G4Event *anEvent) override;
 };
+class Pormes : public G4UImessenger{
+private:
+    PrimaryGen* Pormesclass;
+    G4UIdirectory* myDir;
+    G4UIcmdWithADouble* newPorVal;
+
+public:
+    Pormes(PrimaryGen* cls);
+    ~Pormes();
+    void SetNewValue(G4UIcommand* cmd, G4String nNeutron);
+};
+
+
 #endif //TEMPLATE_PRIMARYGEN_HH
--- src/PrimaryGen.cc
+++ src/PrimaryGen.cc
@@ -5,20 +5,43 @@
 #include <G4Event.hh>
 #include <PrimaryGen.hh>
 #include <G4Gamma.hh>
+#include <G4Neutron.hh>
+#include <G4Electron.hh>
 #include "G4SystemOfUnits.hh"
-void PrimaryGen::GeneratePrimaries(G4Event *anEvent) {
-    gun->GeneratePrimaryVertex(anEvent);
-}
+
 
 PrimaryGen::PrimaryGen() {
+    Pormesclass=new Pormes(this);
     pos_vect.set(0,0,-10*cm);
     gun = new G4ParticleGun(1);
-    gun->SetParticleDefinition(G4Gamma::GammaDefinition());
     gun->SetParticleMomentumDirection(G4ThreeVector(0,0,1));
     gun->SetParticlePosition(pos_vect);
     gun->SetParticleEnergy(661*keV);
 }
+Pormes::Pormes(PrimaryGen* cls):Pormesclass(cls) {
+    myDir = new G4UIdirectory ("/my_dir/");
+    newPorVal = new G4UIcmdWithADouble ("/my_dir/set_new_neutron", this);
+}
+void Pormes::SetNewValue(G4UIcommand* cmd1, G4String nNeutron){
+    if (cmd1 == newPorVal){
+        Pormesclass->Porval(newPorVal -> GetNewDoubleValue(nNeutron));
+    }
+}
+void PrimaryGen::Porval(G4double porval){
+    Neutron = porval;
+}
+void PrimaryGen::GeneratePrimaries(G4Event *anEvent) {
 
+    if(Neutron==1){
+        gun->SetParticleDefinition(G4Neutron::NeutronDefinition());
+    }
+    else{
+        gun->SetParticleDefinition(G4Gamma::GammaDefinition());
+    }
+    gun->GeneratePrimaryVertex(anEvent);
+}
+Pormes::~Pormes(){}
 PrimaryGen::~PrimaryGen() {
     delete gun;
 }
+
